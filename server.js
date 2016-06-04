de1de87e6891153fae42414e197c330422a5aef3
Martin Langdon <martin@netengine.com.au>
2016-06-04 14:21:57 +1000

    server...

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const app         = express();
+const router      = express.Router();
+const port        = 8080;
+
+app.use(express.static(__dirname + '/public'));
+app.use('/api', router);
+
+router.use(bodyParser.json());
+
+router.route('/collection')
+  .post((req, res) => {
+    console.log(req.body);
+    res.json({ response: 'request received!' });
+  });
+
+app.listen(port, () => {
+  console.log(`Now listening on ${ port }`);
+});
