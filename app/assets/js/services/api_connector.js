ad9337d2ccba949b54a9575c2be2612534501d39
Martin Langdon <martin@netengine.com.au>
2016-06-04 15:46:02 +1000

    Simple data connector for api calls.

---
+++ app/assets/js/services/api_connector.js
@@ -0,0 +1,20 @@
+angular.module('app').service('api_connector', ['$http', function($http) {
+
+  var sendCollectionDetails = function sendCollectionDetails(requestBody) {
+
+    requestBody = {
+      // ...test body goes here
+    };
+
+    return $http({
+      method: 'POST',
+      url: 'http://localhost:8080/api/collection',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: requestBody
+    });
+
+  };
+
+}]);
