36c5a94ea515d9404f8b9bca86f876de6089404a
Ben Kolera <ben.kolera@ephox.com>
2016-06-04 16:24:22 +1000

    Firebase for the API

--- server.js
+++ server.js
@@ -2,9 +2,15 @@
 
 const express     = require('express');
 const bodyParser  = require('body-parser');
+const firebase    = require('firebase');
 const app         = express();
 const router      = express.Router();
 const port        = 8081;
+
+firebase.initializeApp({
+    "databaseURL": "https://project-6353258358688553392.firebaseio.com",
+    "serviceAccount": "config/firebase.json"
+});
 
 app.use(express.static(__dirname + '/build'));
 app.use('/api', router);
