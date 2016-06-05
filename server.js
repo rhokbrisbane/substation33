0774eb10f78176b9dcc72c2440c4707e8e5560f4
Martin Langdon <martin@netengine.com.au>
2016-06-05 14:11:36 +1000

    Update with env variables

--- server.js
+++ server.js
@@ -13,7 +13,18 @@ const helpers     = require('./server/helpers.js');
 
 firebase.initializeApp({
   "databaseURL": "https://project-6353258358688553392.firebaseio.com",
-  "serviceAccount": "config/firebase.json"
+  "serviceAccount": {
+    "type": "service_account",
+    "project_id": process.env.PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://accounts.google.com/o/oauth2/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/subsystem33%40project-6353258358688553392.iam.gserviceaccount.com"
+  }
 });
 
 var db = firebase.database();
