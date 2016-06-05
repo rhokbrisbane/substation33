c57492bfedb56af39eab02fae5187a91a890b482
ecoologic <erikecoologic@gmail.com>
2016-06-05 11:33:51 +1000

    send emails

--- server/config.js
+++ server/config.js
@@ -4,7 +4,7 @@ module.exports = {
   port:             8081,
   environment:      process.env.ENVIRONMENT || 'development',
   emailGateway: {
-    sendEmails:     true, // process.env.ENVIRONMENT === 'production',
+    sendEmails:     process.env.ENVIRONMENT === 'production',
     sendgridApikey: process.env.SENDGRID_APIKEY,
     accountEmail:   process.env.ACCOUNT_EMAIL
   }
