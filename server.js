e0376b44eabe12c052687166e4f504c09f066c60
Martin Langdon <martin@netengine.com.au>
2016-06-06 16:55:25 +1000

    added total donation weight check

--- server.js
+++ server.js
@@ -63,14 +63,18 @@ router
     if (key == undefined) {
       firebase.database().ref('/collections/').push(req.body).then(function(result) {
         res.json({ response: 'request received!' });
-        emailGateway.sendThankYouEmail({ to: req.body.customer.email, templateName: 'default' });
+        if (req.body.donation.total_donation_weight > 0) {
+          emailGateway.sendThankYouEmail({ to: req.body.customer.email, templateName: 'default' });
+        }
       });
     } else {
       var updates = {};
       updates['/collections/' + key] = req.body;
       firebase.database().ref().update(updates).then(function(result) {
         res.json({ response: 'request received!' });
-        emailGateway.sendThankYouEmail({ to: req.body.customer.email, templateName: 'default' });
+        if (req.body.donation.total_donation_weight > 0) {
+          emailGateway.sendThankYouEmail({ to: req.body.customer.email, templateName: 'default' });
+        }
       }); 
     }
     
